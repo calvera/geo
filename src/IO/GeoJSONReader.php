3876434d153fff38bcc478f0494d2987ee8c369d
Karel Sommer <karel.sommer@gmail.com>
2019-09-16 13:26:56 +0200

    make readGeoJSON public

--- src/IO/GeoJSONReader.php
+++ src/IO/GeoJSONReader.php
@@ -83,7 +83,7 @@ class GeoJSONReader
      *
      * @throws GeometryException If the GeoJSON file is invalid.
      */
-    private function readGeoJSON(array $geojson) : Geometry
+    public function readGeoJSON(array $geojson) : Geometry
     {
         if (! isset($geojson['type']) || ! is_string($geojson['type'])) {
             throw GeometryIOException::invalidGeoJSON('Missing or malformed "type" attribute.');
